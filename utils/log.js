2206423d8e95a9c281ce63328ace33a507dbe931
cv281115-arsh <cv281115@gmail.com>
2026-02-21 21:43:16 +0530

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -24,13 +24,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
   switch (option) {
     case "warn":
-      console.log(chalk.bold.hex(randomColor()).bold(" [ ARYAN ❤️ ] ") + chalk.bold.hex("#8B8878").bold(data)+chalk.bold.hex("FF00DD")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ LEGEND ARYAN ❤️ (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ))
+      console.log(chalk.bold.hex(randomColor()).bold(" [ ARSH😘 ] ") + chalk.bold.hex("#8B8878").bold(data)+chalk.bold.hex("FF00DD")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ ARSH KING ❤️ (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ))
       break;
     case "error":
-    console.log(chalk.bold.hex(randomColor()).bold(" [ ARYAN ❤️ ] ") + data+chalk.bold.hex("5EFF00")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ LEGEND ARYAN ❤️ (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ));
+    console.log(chalk.bold.hex(randomColor()).bold(" [ ARSH😘 ] ") + data+chalk.bold.hex("5EFF00")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ ARSH KING ❤️ (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ));
       break;
     default:
-      console.log(chalk.bold.hex(randomColor()).bold(" [ ARYAN ❤️ ] ") + chalk.bold.hex(randomColor()).bold(data) +chalk.bold.hex("FFF0000")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ LEGEND ARYAN ❤️ (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ));
+      console.log(chalk.bold.hex(randomColor()).bold(" [ ARSH😘 ] ") + chalk.bold.hex(randomColor()).bold(data) +chalk.bold.hex("FFF0000")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ ARSH KING  ❤️ (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ));
       break;
   }
 }
